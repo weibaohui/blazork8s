cbe122ca9e4f02a3ed43343a5c0445bc611ced85
weibaohui <weibaohui@yeah.net>
2021-06-15 00:13:38 +0800

    更新为bootstrap

--- ui/Program.cs
+++ ui/Program.cs
@@ -12,7 +12,7 @@ namespace ui
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.Services.AddAntDesign();
+            builder.Services.AddBootstrapBlazor();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
