0fb4ece8fe49b421a8954bdcf961248b8c8176b9
weibaohui <weibaohui@yeah.net>
2021-07-16 14:26:07 +0800

    6.0

--- server/Startup.cs
+++ server/Startup.cs
@@ -1,19 +1,13 @@
 using System;
-using System.Collections.Generic;
 using System.Data.Common;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using server.Service.K8s;
-using server.Utils;
 
 namespace server
 {
