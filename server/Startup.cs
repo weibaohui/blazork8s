9a7031757f7ff37fa4e46d658cdec51fcd93a799
weibaohui <weibaohui@yeah.net>
2021-07-17 15:08:53 +0800

    update

--- server/Startup.cs
+++ server/Startup.cs
@@ -28,7 +28,7 @@ namespace server
             #region db
 
             IFreeSql fsql = new FreeSql.FreeSqlBuilder()
-                .UseConnectionString(FreeSql.DataType.Sqlite, @"Data Source=db1.db")
+                .UseConnectionString(FreeSql.DataType.PostgreSQL, @"Host=127.0.0.1;Port=5432;Username=postgres;Password=postgres; Database=blazork8s;Pooling=true;Minimum Pool Size=1")
                 .UseAutoSyncStructure(true) //自动同步实体结构到数据库，FreeSql不会扫描程序集，只有CRUD时才会生成表。
                 .UseMonitorCommand(cmd =>
                 {
