3ce86dde34a900fc5519eeae0a723b32c752f796
weibaohui <weibaohui@yeah.net>
2021-08-15 15:30:52 +0800

    node 节点状态

--- Extension/k8s/NodeExtension.cs
+++ Extension/k8s/NodeExtension.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using k8s.Models;
-using Microsoft.VisualBasic;
 
 namespace Extensions.k8s
 {
@@ -12,12 +10,18 @@ namespace Extensions.k8s
         {
             foreach (var condition in conditions)
             {
-                if (condition.Type == "Ready" && condition.Status=="True" )
+                if (condition.Type == "Ready" && condition.Status == "True")
                 {
                     return true;
                 }
             }
+
             return false;
+        }
+
+        public static string? NotReadyReason(this IList<V1NodeCondition> conditions)
+        {
+            return conditions.FirstOrDefault(w => w.Type == "Ready")?.Reason?.ToString();
         }
 
         /// <summary>
@@ -32,6 +36,5 @@ namespace Extensions.k8s
                 .Select(d => d.Key.Replace("node-role.kubernetes.io/", ""))
                 .ToList();
         }
-
     }
 }
