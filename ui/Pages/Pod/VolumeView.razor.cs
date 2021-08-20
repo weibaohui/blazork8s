a26180a1563813d11dda2cef112c13f244a6e9d9
weibaohui <weibaohui@yeah.net>
2021-08-21 00:08:45 +0800

    Volume

---
+++ ui/Pages/Pod/VolumeView.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ui.Pages.Pod
+{
+    public partial class VolumeView : ComponentBase
+    {
+        [Parameter]
+        public  V1Pod  PodItem { get; set; }
+    }
+}
