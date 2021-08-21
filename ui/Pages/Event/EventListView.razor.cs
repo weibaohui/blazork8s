444b3e47e7b3711a9cf86ec7365c480b4d20f51f
weibaohui <weibaohui@yeah.net>
2021-08-21 23:53:52 +0800

    display

--- ui/Pages/Event/EventListView.razor.cs
+++ ui/Pages/Event/EventListView.razor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
 namespace ui.Pages.Event
@@ -9,5 +10,9 @@ namespace ui.Pages.Event
   
         [Parameter]
         public IList<Corev1Event> Events { get; set; }
+        protected override void OnInitialized()
+        {
+            Events = Events.OrderByDescending(e => e.LastTimestamp).ToList();
+        }
     }
 }
