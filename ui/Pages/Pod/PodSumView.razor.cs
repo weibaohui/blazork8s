2262b61efc8e9148a94bc0e4047f3428c465fbff
weibaohui <weibaohui@yeah.net>
2021-08-21 21:58:49 +0800

    快捷显示

--- ui/Pages/Pod/PodSumView.razor.cs
+++ ui/Pages/Pod/PodSumView.razor.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AntDesign;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
 
@@ -14,9 +15,24 @@ namespace ui.Pages.Pod
         public IList<V1Pod> Pods { get; set; }
 
 
+        [Inject]
+        private DrawerService DrawerService { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
+        }
+
+        async Task OnPodClick(V1Pod pod)
+        {
+            var options = new DrawerOptions
+            {
+                Title = "POD:" + pod.Name(),
+                Width = 800
+            };
+            var drawerRef =
+                await DrawerService.CreateAsync<PodDetailView, V1Pod, bool>(options,
+                    pod);
         }
     }
 }
