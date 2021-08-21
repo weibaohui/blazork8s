37bebada040491014a1cdc70ca2549fb3e41db16
weibaohui <weibaohui@yeah.net>
2021-08-21 23:33:59 +0800

    update event display

---
+++ ui/Pages/Event/EventListView.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+namespace ui.Pages.Event
+{
+    public partial class EventListView : ComponentBase
+    {
+  
+        [Parameter]
+        public IList<Corev1Event> Events { get; set; }
+    }
+}
