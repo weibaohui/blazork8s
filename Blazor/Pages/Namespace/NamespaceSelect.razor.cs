fec289ca6a53cffecbc9c80117545d5f23eb9563
weibaohui <weibaohui@yeah.net>
2021-08-22 22:25:50 +0800

    ns

---
+++ Blazor/Pages/Namespace/NamespaceSelect.razor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blazor.Service;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor.Pages.Namespace
+{
+    public partial class NamespaceSelect:ComponentBase
+    {
+        [Parameter]
+        public EventCallback<String> OnNsSelected { get; set; }
+
+        [Inject]
+        private INamespaceService NamespaceService { get; set; }
+
+        private IList<V1Namespace> ns;
+        protected override async Task OnInitializedAsync()
+        {
+             ns = await NamespaceService.GetNamespaces();
+           
+        }
+
+        void handleChange(string value)
+        {
+            OnNsSelected.InvokeAsync(value);
+            Console.WriteLine(value);
+        }
+
+        void handleItemsChange(IEnumerable<string> value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
