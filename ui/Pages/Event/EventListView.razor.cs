ddf8b1dd3b650688aac1eaf4600115c468a073a6
weibaohui <weibaohui@yeah.net>
2021-08-22 11:39:28 +0800

    Event

--- ui/Pages/Event/EventListView.razor.cs
+++ ui/Pages/Event/EventListView.razor.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
 namespace ui.Pages.Event
 {
     public partial class EventListView : ComponentBase
     {
-  
         [Parameter]
         public IList<Corev1Event> Events { get; set; }
         protected override void OnInitialized()
         {
-            Events = Events.OrderByDescending(e => e.LastTimestamp).ToList();
+            Events = Events.OrderByDescending(w=>w.Type).OrderByDescending(e => e.LastTimestamp).ToList();
+            this.StateHasChanged();
         }
     }
 }
