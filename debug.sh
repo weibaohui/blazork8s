d0197bf5cc98bdc8d08ddd68e9b3187131451c63
weibaohui <weibaohui@yeah.net>
2021-09-11 23:05:11 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -1,3 +1,6 @@
 #Debug
 #start server using port 4000
 docker run -it --rm -v ~/.kube/:/root/.kube/ -p 4001:443 -p 4000:80 ghcr.io/weibaohui/blazork8s:latest
+#always run server
+docker stop blazork8s && docker rm blazork8s
+docker run -d --name blazork8s  -v ~/.kube/:/root/.kube/ -p 4001:443 -p 4000:80 ghcr.io/weibaohui/blazork8s:latest
