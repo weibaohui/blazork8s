e1758545445bf06742f470fa77f65e75a078363e
weibaohui <weibaohui@yeah.net>
2021-09-20 16:03:45 +0800

    rs

---
+++ Blazor/Pages/ReplicaSet/ReplicaSetIndex.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Pages.ReplicaSet
+{
+    public partial class ReplicaSetIndex: ComponentBase
+    {
+        public ReplicaSetIndex()
+        {
+        }
+    }
+}
