ae9a940e81e12470e80d6e1588a7d35b5d42246e
weibaohui <weibaohui@yeah.net>
2022-11-26 21:06:02 +0800

    clean

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -2,14 +2,12 @@ using System;
 using AntDesign.ProLayout;
 using BlazorApp.Service;
 using BlazorApp.Service.impl;
-using k8s;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
