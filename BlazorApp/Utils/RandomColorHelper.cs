0b272b07fdedde6eded5a8b745ba3fa0d77ea7db
weibaohui <weibaohui@yeah.net>
2023-12-13 22:43:40 +0800

    color

--- BlazorApp/Utils/RandomColorHelper.cs
+++ BlazorApp/Utils/RandomColorHelper.cs
@@ -1,10 +1,27 @@
-using AntDesign;
 using Extension;
 
 namespace BlazorApp.Utils;
 
+/// <summary>
+/// 相比preset color ，去掉了浅色
+/// </summary>
+public enum RandomColor {
+    Pink,
+    Red,
+    // Yellow,
+    Orange,
+    Cyan,
+    Green,
+    Blue,
+    Purple,
+    GeekBlue,
+    Magenta,
+    Volcano,
+    Gold,
+    // Lime
+}
 public static class RandomColorHelper
 {
-    public static string RandomColor() => typeof(PresetColor).GetRandomEnumValue()?.ToString();
-    public static string RandomInverseColor() => typeof(PresetColor).GetRandomEnumValue()?.ToString().ToLower()+"-inverse";
+    public static string RandomColor() => typeof(RandomColor).GetRandomEnumValue()?.ToString();
+    public static string RandomInverseColor() => typeof(RandomColor).GetRandomEnumValue()?.ToString().ToLower()+"-inverse";
 }
