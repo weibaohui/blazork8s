583872e536cf94b2d4a8da675ed8fd6d23d36fe9
weibaohui <weibaohui@yeah.net>
2023-12-14 15:19:26 +0800

    service detail

--- BlazorApp/Pages/Service/ServiceDetailView.razor.cs
+++ BlazorApp/Pages/Service/ServiceDetailView.razor.cs
@@ -1,18 +1,32 @@
 using System.Threading.Tasks;
+using AntDesign;
 using BlazorApp.Pages.Common;
+using BlazorApp.Utils.PortForwarding;
+using Entity;
 using k8s.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp.Pages.Service
 {
     public partial class ServiceDetailView : DrawerPageBase<V1Service>
     {
 
+        [Inject]
+        IMessageService MessageService { get; set; }
         private V1Service Item { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             Item = base.Options;
             await base.OnInitializedAsync();
+        }
+
+        private async Task Forward(V1ServicePort p)
+        {
+            await PortForwardExecutorHelper.Instance.ForwardPort(PortForwardType.Svc,Item.Namespace(), Item.Name(),
+                p.Port.ToString(),
+                PortForward.RandomPort());
+            await MessageService.Success("设置 转发成功 !");
         }
     }
 }
