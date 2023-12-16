d31a84a7b76cd323a870602ed01a4dacf1e9ae08
weibaohui <weibaohui@yeah.net>
2023-12-16 10:20:57 +0800

    update

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using Generator;
-using k8s;
-using k8s.Models;
 
 class Program
 {
@@ -66,51 +61,8 @@ class Program
     public static void Main()
     {
         // new Program().makeTemplate();
-        PrepareK8SEntity();
+        // EntityPrepare.PrepareK8SEntity();
+        // RazorEngineProcessor.Process();
     }
 
-
-    /// <summary>
-    /// 准备k8s资源，为下一步生成字段明细做准备
-    /// </summary>
-    private static void PrepareK8SEntity()
-    {
-        var kubeList = new List<KubeType>();
-        ReflectHelper<V1Pod>.ListProperty(typeof(V1Pod), "Pod", kubeList);
-        var s = KubernetesJson.Serialize(kubeList);
-        File.WriteAllText("pod.json", s);
-
-        var zList = new List<KubeType>();
-        ZipList(zList, kubeList);
-        zList.Sort((a, b) => a.FieldLevel - b.FieldLevel);
-        zList.Sort((a, b) => String.CompareOrdinal(a.FullName, b.FullName));
-
-        File.WriteAllText("pod-zip.json",
-            KubernetesJson.Serialize(zList
-                .Where(x => x.FieldLevel == 3)
-                .ToList()
-            )
-        );
-    }
-
-
-    /// <summary>
-    /// 将列表中的child抽取打平为一个扁平list
-    /// </summary>
-    /// <param name="topList"></param>
-    /// <param name="childList"></param>
-
-    private static void ZipList(IList<KubeType> topList, IList<KubeType> childList)
-    {
-        foreach (var kubeType in childList)
-        {
-            if (kubeType.Child != null)
-            {
-                ZipList(topList, kubeType.Child);
-            }
-
-            kubeType.Child = null;
-            topList.Add(kubeType);
-        }
-    }
 }
