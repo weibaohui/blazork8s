44fff80e3b41d7c1efcf820a3cc01d1b4f2d3f07
weibaohui <weibaohui@yeah.net>
2023-12-27 11:29:12 +0800

    pod port forward

--- BlazorApp/Pages/Pod/PodSpecView.razor.cs
+++ BlazorApp/Pages/Pod/PodSpecView.razor.cs
@@ -1,8 +1,11 @@
 using System.Threading.Tasks;
+using AntDesign;
 using BlazorApp.Pages.Common;
 using BlazorApp.Pages.Node;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils;
+using BlazorApp.Utils.PortForwarding;
+using Entity;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
 
@@ -10,10 +13,19 @@ namespace BlazorApp.Pages.Pod;
 
 public partial class PodSpecView : PageBase
 {
+    [Inject]
+    IMessageService MessageService { get; set; }
+
     [Parameter]
     public V1PodSpec PodSpec { get; set; }
     [Inject]
     private INodeService NodeService { get; set; }
+
+
+    [Parameter]
+    public string PodNamespace { get; set; }
+    [Parameter]
+    public string PodName { get; set; }
     protected override async Task OnInitializedAsync()
     {
 
@@ -26,5 +38,13 @@ public partial class PodSpecView : PageBase
         await PageDrawerHelper<V1Node>.Instance
             .SetDrawerService(PageDrawerService.DrawerService)
             .ShowDrawerAsync<NodeDetailView, V1Node, bool>(node);
+    }
+
+    public async Task OnPortForward(int port)
+    {
+        await PortForwardExecutorHelper.Instance.ForwardPort(PortForwardType.Pod, PodNamespace, PodName,
+            port.ToString(),
+            PortForward.RandomPort());
+        await MessageService.Success("设置 转发成功 !");
     }
 }
