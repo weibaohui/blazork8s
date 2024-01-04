dfe4eb7e06c37c1f913fc11abd2d8d9da2ef93f0
weibaohui <weibaohui@yeah.net>
2024-01-04 11:22:50 +0800

    service account roles

--- BlazorApp/Pages/ServiceAccount/ServiceAccountDetailView.razor.cs
+++ BlazorApp/Pages/ServiceAccount/ServiceAccountDetailView.razor.cs
@@ -1,15 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using k8s.Models;
 using BlazorApp.Pages.Common;
+using BlazorApp.Service.k8s;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Pages.ServiceAccount
 {
-    public partial class ServiceAccountDetailView :  DrawerPageBase<V1ServiceAccount>
+    public partial class ServiceAccountDetailView : DrawerPageBase<V1ServiceAccount>
     {
+        [Inject]
+        public IRoleService RoleService { get; set; }
+
+        [Inject]
+        public IRoleBindingService RoleBindingService { get; set; }
+
         private V1ServiceAccount ServiceAccount { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             ServiceAccount = base.Options;
             await base.OnInitializedAsync();
+        }
+
+
+        private List<V1RoleRef> ListSaRoles()
+        {
+            var saName = ServiceAccount.Name();
+            var bindings = RoleBindingService.List()
+                .Where(x =>
+                    x.Subjects is { Count: > 0 }
+                    && x.Subjects.Any(y => y.Kind == "ServiceAccount" && y.Name == saName)
+                )
+                .ToList();
+            return bindings.Select(x => x.RoleRef).ToList();
         }
     }
 }
