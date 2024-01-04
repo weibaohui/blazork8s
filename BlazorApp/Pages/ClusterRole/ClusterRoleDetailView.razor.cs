00f8f6e9e4f95be2c019eb35271dbf92caa31b4f
weibaohui <weibaohui@yeah.net>
2024-01-04 14:50:04 +0800

    cluster role

--- BlazorApp/Pages/ClusterRole/ClusterRoleDetailView.razor.cs
+++ BlazorApp/Pages/ClusterRole/ClusterRoleDetailView.razor.cs
@@ -1,15 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using k8s.Models;
 using BlazorApp.Pages.Common;
+using BlazorApp.Service.k8s;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Pages.ClusterRole
 {
-    public partial class ClusterRoleDetailView :  DrawerPageBase<V1ClusterRole>
+    public partial class ClusterRoleDetailView : DrawerPageBase<V1ClusterRole>
     {
+        [Inject]
+        public IClusterRoleBindingService ClusterRoleBindingService { get; set; }
+
         private V1ClusterRole ClusterRole { get; set; }
+
+        private List<V1Subject> Subjects { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             ClusterRole = base.Options;
+            Subjects    = ListServiceAccount();
             await base.OnInitializedAsync();
+        }
+
+
+        private List<V1Subject> ListServiceAccount()
+        {
+            var name = ClusterRole.Name();
+            var bindings = ClusterRoleBindingService.List()
+                .Where(x =>
+                    x.RoleRef is not null && x.RoleRef.Name == name
+                )
+                .ToList();
+            return bindings.SelectMany(x => x.Subjects).ToList();
         }
     }
 }
--- BlazorApp/Pages/Role/RoleDetailView.razor.cs
+++ BlazorApp/Pages/Role/RoleDetailView.razor.cs
@@ -1,15 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using k8s.Models;
 using BlazorApp.Pages.Common;
+using BlazorApp.Service.k8s;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Pages.Role
 {
-    public partial class RoleDetailView :  DrawerPageBase<V1Role>
+    public partial class RoleDetailView : DrawerPageBase<V1Role>
     {
-        private V1Role Role { get; set; }
+        [Inject]
+        public IRoleBindingService RoleBindingService { get; set; }
+
+
+        private List<V1Subject> Subjects { get; set; }
+        private V1Role          Role     { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             Role = base.Options;
+            Subjects= ListServiceAccount();
             await base.OnInitializedAsync();
+        }
+
+
+        private List<V1Subject> ListServiceAccount()
+        {
+            var bindings = RoleBindingService.List()
+                .Where(x =>
+                    x.Namespace()== Role.Namespace() &&
+                    x.RoleRef is not null && x.RoleRef.Name == Role.Name()
+                )
+                .ToList();
+            return bindings.SelectMany(x => x.Subjects).ToList();
         }
     }
 }
