1317aaf00fa83d508e959d03faa8b7787c702267
weibaohui <weibaohui@yeah.net>
2024-01-05 09:40:58 +0800

    update

--- BlazorApp/Pages/EndpointSlice/EndpointSliceIndex.razor.cs
+++ BlazorApp/Pages/EndpointSlice/EndpointSliceIndex.razor.cs
@@ -1,30 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using BlazorApp.Pages.Common;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Pages.EndpointSlice;
+
 public partial class EndpointSliceIndex : TableBase<V1EndpointSlice>
 {
     [Inject]
     private IEndpointSliceService EndpointSliceService { get; set; }
+
     private async Task OnResourceChanged(ResourceCache<V1EndpointSlice> data)
     {
         ItemList = data;
         TableData.CopyData(ItemList);
         await InvokeAsync(StateHasChanged);
     }
+
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
         TableData.CopyData(ItemList);
         await InvokeAsync(StateHasChanged);
     }
+
     private async Task OnItemNameClick(V1EndpointSlice item)
     {
         await PageDrawerHelper<V1EndpointSlice>.Instance
             .SetDrawerService(PageDrawerService.DrawerService)
             .ShowDrawerAsync<EndpointSliceDetailView, V1EndpointSlice, bool>(item);
+    }
+
+    private IList<string> GetEndPointsIpPorts(V1EndpointSlice es)
+    {
+        var ret = new List<string>();
+        if (es.Endpoints is null) return ret;
+        if (es.Ports is not { Count: > 0 }) return ret;
+        foreach (var port in es.Ports)
+        {
+            es.Endpoints
+                .Where(x => x.Addresses is { Count: > 0 })
+                .ToList()
+                .SelectMany(x => x.Addresses)
+                .ToList()
+                .ForEach(x => ret.Add($"{x}:{port.Port}/{port.Protocol}"));
+        }
+
+        return ret;
     }
 }
--- BlazorApp/Pages/Endpoints/EndpointsIndex.razor.cs
+++ BlazorApp/Pages/Endpoints/EndpointsIndex.razor.cs
@@ -1,30 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using BlazorApp.Pages.Common;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Pages.Endpoints;
+
 public partial class EndpointsIndex : TableBase<V1Endpoints>
 {
     [Inject]
     private IEndpointsService EndpointsService { get; set; }
+
     private async Task OnResourceChanged(ResourceCache<V1Endpoints> data)
     {
         ItemList = data;
         TableData.CopyData(ItemList);
         await InvokeAsync(StateHasChanged);
     }
+
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
         TableData.CopyData(ItemList);
         await InvokeAsync(StateHasChanged);
     }
+
     private async Task OnItemNameClick(V1Endpoints item)
     {
         await PageDrawerHelper<V1Endpoints>.Instance
             .SetDrawerService(PageDrawerService.DrawerService)
             .ShowDrawerAsync<EndpointsDetailView, V1Endpoints, bool>(item);
+    }
+
+    private IList<string> GetEndPointsIpPorts(V1Endpoints ep)
+    {
+        var ret = new List<string>();
+        if (ep.Subsets is null) return ret;
+
+        ep.Subsets.Where(x => x.Ports is { Count: > 0 } && x.Addresses is { Count: > 0 })
+            .ToList()
+            .ForEach(x =>
+            {
+                x.Ports.ToList()
+                    .ForEach(p => { ret.AddRange(x.Addresses.Select(m => $"{m.Ip}:{p.Port}/{p.Protocol}")); });
+            });
+        return ret;
     }
 }
