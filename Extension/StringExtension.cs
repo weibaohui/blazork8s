ca5828754392cf3dcc3931a98b30400df9373c13
weibaohui <weibaohui@yeah.net>
2024-01-05 14:07:59 +0800

    update

--- Extension/StringExtension.cs
+++ Extension/StringExtension.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Extension;
 
@@ -11,7 +14,37 @@ public static class StringExtensions
                    .Replace(" ", "&nbsp;&nbsp;")
                ?? string.Empty;
     }
+    public static string ToMd5Str(this string input)
+    {
+        // Use input string to calculate MD5 hash
+        MD5    md5        = MD5.Create();
+        var inputBytes = Encoding.ASCII.GetBytes(input);
+        var hashBytes  = md5.ComputeHash(inputBytes);
 
+        // Convert the byte array to hexadecimal string
+        StringBuilder sb = new StringBuilder();
+        foreach (var t in hashBytes)
+        {
+            sb.Append(t.ToString("X2"));
+            // To force the hex string to lower-case letters instead of
+            // upper-case, use he following line instead:
+            // sb.Append(hashBytes[i].ToString("x2"));
+        }
+        return sb.ToString();
+    }
+    public static long ToMd5Digits(this string str)
+    {
+        return Convert.ToInt64(str.ToMd5Str().RemoveStringOfNonDigits());
+    }
+    /// <summary>
+    /// 去除非数字字符，只保留数字
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static string RemoveStringOfNonDigits(this string str)
+    {
+        return new string(str.Where(char.IsDigit).ToArray());
+    }
 
     /// <summary>
     /// 字符串转换为下划线
