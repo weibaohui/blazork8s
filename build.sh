74016a80594a11cee270cb9a4e88627758f5ee7b
weibaohui <weibaohui@yeah.net>
2024-01-09 11:27:47 +0800

    buildx update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #login
 echo $CR_PAT | docker login ghcr.io -u weibaohui --password-stdin
 
-docker build -t ghcr.io/weibaohui/blazork8s:latest -f BlazorApp/Dockerfile .
-docker push ghcr.io/weibaohui/blazork8s:latest
-#docker buildx build -t  ghcr.io/weibaohui/blazork8s-cross:latest  -f BlazorApp/Dockerfile   --platform=linux/arm64,linux/amd64 . --push
+#docker build -t ghcr.io/weibaohui/blazork8s:latest -f BlazorApp/Dockerfile .
+#docker push ghcr.io/weibaohui/blazork8s:latest
+docker buildx build -t  ghcr.io/weibaohui/blazork8s:latest  -f BlazorApp/Dockerfile   --platform=linux/arm64,linux/amd64 . --push
