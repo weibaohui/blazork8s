3bd8b2631fe58796d622834d7ff2013e3cde51df
weibaohui <weibaohui@yeah.net>
2024-01-23 20:57:15 +0800

    clean

--- BlazorApp/Pages/Ingress/IngressDetailView.razor.cs
+++ BlazorApp/Pages/Ingress/IngressDetailView.razor.cs
@@ -1,15 +1,15 @@
 using System.Threading.Tasks;
-using k8s.Models;
 using BlazorApp.Pages.Common;
-namespace BlazorApp.Pages.Ingress
+using k8s.Models;
+
+namespace BlazorApp.Pages.Ingress;
+
+public partial class IngressDetailView :  DrawerPageBase<V1Ingress>
 {
-    public partial class IngressDetailView :  DrawerPageBase<V1Ingress>
+    private V1Ingress Ingress { get; set; }
+    protected override async Task OnInitializedAsync()
     {
-        private V1Ingress Ingress { get; set; }
-        protected override async Task OnInitializedAsync()
-        {
-            Ingress = base.Options;
-            await base.OnInitializedAsync();
-        }
+        Ingress = base.Options;
+        await base.OnInitializedAsync();
     }
 }
