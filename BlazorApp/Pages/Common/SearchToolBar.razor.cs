8190c22c2a5c181045f608ec6be7ccdac605eb6a
weibaohui <weibaohui@yeah.net>
2024-01-24 11:08:25 +0800

    ns select fix

--- BlazorApp/Pages/Common/SearchToolBar.razor.cs
+++ BlazorApp/Pages/Common/SearchToolBar.razor.cs
@@ -1,32 +1,34 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
-namespace BlazorApp.Pages.Common
+namespace BlazorApp.Pages.Common;
+
+public partial class SearchToolBar : ComponentBase
 {
-    public partial class SearchToolBar : ComponentBase
+    [Parameter]
+    public string Title { get; set; }
+
+    [Parameter]
+    public int Count { get; set; }
+
+    [Parameter]
+    public bool HideNsSelector { get; set; } = false;
+
+    [Parameter]
+    public EventCallback<string> OnNsSelected { get; set; }
+
+    [Parameter]
+    public EventCallback<string> OnSearch { get; set; }
+
+    private string TxtValue { get; set; }
+
+    private void OnNsSelectedHandler(string ns)
     {
-        [Parameter]
-        public string Title { get; set; }
+        OnNsSelected.InvokeAsync(ns);
+    }
 
-        [Parameter]
-        public int Count { get; set; }
-
-        [Parameter]
-        public EventCallback<string> OnNsSelected { get; set; }
-
-        [Parameter]
-        public EventCallback<string> OnSearch { get; set; }
-
-        private string TxtValue { get; set; }
-
-        private void OnNsSelectedHandler(string ns)
-        {
-            OnNsSelected.InvokeAsync(ns);
-        }
-
-        private async Task OnSearchHandler()
-        {
-            await OnSearch.InvokeAsync(TxtValue);
-        }
+    private async Task OnSearchHandler()
+    {
+        await OnSearch.InvokeAsync(TxtValue);
     }
 }
