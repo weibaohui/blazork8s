ce0c979cf3c70f2f55db0cce0b45c78989546621
weibaohui <weibaohui@yeah.net>
2024-01-30 15:26:38 +0800

    Refactor KubectlExplainView: Update AI translation request text, add retranslation function and button

--- BlazorApp/Pages/Common/Metadata/KubectlExplainView.razor.cs
+++ BlazorApp/Pages/Common/Metadata/KubectlExplainView.razor.cs
@@ -33,7 +33,7 @@ public partial class KubectlExplainView : DrawerPageBase<string>
         if (AiService.Enabled())
         {
             AiService.SetChatEventHandler(EventHandler);
-            _resultCn=await AiService.AIChat("请详细翻译下这段文字，不要遗漏细节：" + _result);
+            _resultCn=await AiService.AIChat("请逐字翻译以下内容，注意请不要遗漏细节并保持原来的格式：" + _result);
         }
     }
 
@@ -42,5 +42,12 @@ public partial class KubectlExplainView : DrawerPageBase<string>
     {
         _resultCn += resp;
         await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task ReTranslate()
+    {
+        _resultCn = "";
+        await InvokeAsync(StateHasChanged);
+        _resultCn = await AiService.AIChat("请详细翻译下这段文字，不要遗漏细节：" + _result);
     }
 }
