4d8d3bf034ad5a23dd936627b41fa4b20691e479
weibaohui <weibaohui@yeah.net>
2024-01-31 16:08:41 +0800

    Refactor ClusterConfig.razor: Simplify layout, replace grid view with NodeStatusChart for Nodes tab, and improve code readability

---
+++ BlazorApp/Pages/Node/NodeStatusChart.razor.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Timers;
+using AntDesign.Charts;
+using BlazorApp.Service.k8s;
+using k8s.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages.Node;
+
+public partial class NodeStatusChart : ComponentBase, IDisposable
+{
+    [Parameter]
+    public V1Node Node { get; set; }
+
+    [Inject]
+    public INodeService NodeService { get; set; }
+
+    [Inject]
+    public IPodService PodService { get; set; }
+
+    private BulletConfig _uiConfig;
+    private Timer        _timer;
+    private IList<V1Pod> podsOnNode;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _timer         =  new Timer(3000);
+        _timer.Elapsed += async (sender, eventArgs) =>await OnTimerCallback();
+        _timer.Start();
+        _uiConfig  = GetConfig();
+        podsOnNode = PodService.ListByNodeName(Node.Name());
+        await base.OnInitializedAsync();
+
+    }
+
+    private async Task OnTimerCallback()
+    {
+        _uiConfig  = GetConfig();
+        podsOnNode = PodService.ListByNodeName(Node.Name());
+       await InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        _timer.Dispose();
+    }
+
+    private int CalcMeasure(string fullValue)
+    {
+        var    strings = fullValue.Split("/");
+        var avg     = Math.Round( (double.Parse(strings[0]) / double.Parse(strings[1])), 2);
+        return (int)(avg*100);
+    }
+
+    BulletConfig GetConfig()
+    {
+        var nodeName       = Node.Name();
+        var podCapacity    = NodeService.GetPodCapacity(nodeName);
+        var cpuCapacity    = NodeService.GetCpuCapacity(nodeName);
+        var memoryCapacity = NodeService.GetMemoryCapacity(nodeName);
+
+        return new BulletConfig
+        {
+            Width       = 300,
+            Height      = 150,
+            RangeMax    = 100,
+            RangeColors = new string[] { "#9ebc19", "#e18a3b", "#c12c1f" },
+            Data = new[]
+            {
+                new BulletViewConfigData
+                {
+                    Title    = "Pods",
+                    Measures = new int[] { CalcMeasure(podCapacity) },
+                    Ranges   = new double[] { 0, 0.5, 0.8, 1 },
+                },
+                new BulletViewConfigData
+                {
+                    Title    = "Memory",
+                    Measures = new int[] {  CalcMeasure(memoryCapacity) },
+                    Ranges   = new double[] { 0, 0.5, 0.8, 1 },
+                },
+                new BulletViewConfigData
+                {
+                    Title    = "CPU",
+                    Measures = new int[] { CalcMeasure(cpuCapacity) },
+                    Ranges   = new double[] { 0, 0.5, 0.8, 1 },
+                }
+            }
+        };
+    }
+}
