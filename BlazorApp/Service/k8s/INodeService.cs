883e876f4976e946af71c77b7fde640552b530a2
weibaohui <weibaohui@yeah.net>
2024-01-31 10:59:20 +0800

    Refactor ClusterConfig.razor and NodeService: Add Pods column to grid, integrate pod count and capacity functionality, and update UI layout

--- BlazorApp/Service/k8s/INodeService.cs
+++ BlazorApp/Service/k8s/INodeService.cs
@@ -4,4 +4,6 @@ namespace BlazorApp.Service.k8s;
 
 public interface INodeService : ICommonAction<V1Node>
 {
+    long   GetPodCount(string         nodeName);
+    string GetPodCapacityCount(string nodeName);
 }
--- BlazorApp/Service/k8s/impl/NodeService.cs
+++ BlazorApp/Service/k8s/impl/NodeService.cs
@@ -1,21 +1,28 @@
+using System.Linq;
 using System.Threading.Tasks;
 using k8s;
 using k8s.Models;
 
 namespace BlazorApp.Service.k8s.impl
 {
-    public class NodeService : CommonAction<V1Node>, INodeService
+    public class NodeService(IKubeService kubeService, IPodService podService) : CommonAction<V1Node>, INodeService
     {
-        private readonly IKubeService _kubeService;
-
-        public NodeService(IKubeService kubeService)
-        {
-            _kubeService = kubeService;
-        }
-
         public new async Task<V1Status> Delete(string ns, string name)
         {
-           return await _kubeService.Client().DeleteNodeAsync(name: name);
+            return await kubeService.Client().DeleteNodeAsync(name: name);
+        }
+
+        public long GetPodCount(string nodeName)
+        {
+            return podService.ListByNodeName(nodeName).ToList().Count;
+        }
+
+        public string GetPodCapacityCount(string nodeName)
+        {
+            var capacity = this.List().FirstOrDefault(x => x.Name() == nodeName)?.Status.Capacity;
+            var pods     = capacity?.FirstOrDefault(x => x.Key == "pods").Value.CanonicalizeString();
+            var current  = GetPodCount(nodeName);
+            return $"{current}/{pods}";
         }
     }
 }
