31cfac83ea7e6258862a636bb2be0e92f21c1313
weibaohui <weibaohui@yeah.net>
2024-01-31 11:27:38 +0800

    Refactor NodeService.cs and ClusterConfig.razor: Add CPU and Memory capacity functionality, update UI layout, and enhance resource management

--- BlazorApp/Service/k8s/INodeService.cs
+++ BlazorApp/Service/k8s/INodeService.cs
@@ -5,5 +5,7 @@ namespace BlazorApp.Service.k8s;
 public interface INodeService : ICommonAction<V1Node>
 {
     long   GetPodCount(string         nodeName);
-    string GetPodCapacityCount(string nodeName);
+    string GetPodCapacity(string nodeName);
+    string GetMemoryCapacity(string   nodeName);
+    string GetCpuCapacity(string      nodeName);
 }
--- BlazorApp/Service/k8s/impl/NodeService.cs
+++ BlazorApp/Service/k8s/impl/NodeService.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Extension;
 using k8s;
 using k8s.Models;
 
 namespace BlazorApp.Service.k8s.impl
 {
-    public class NodeService(IKubeService kubeService, IPodService podService) : CommonAction<V1Node>, INodeService
+    public class NodeService(IKubeService kubeService, IPodService podService, IMetricsService MetricsService)
+        : CommonAction<V1Node>, INodeService
     {
         public new async Task<V1Status> Delete(string ns, string name)
         {
@@ -17,12 +20,42 @@ namespace BlazorApp.Service.k8s.impl
             return podService.ListByNodeName(nodeName).ToList().Count;
         }
 
-        public string GetPodCapacityCount(string nodeName)
+        public string GetPodCapacity(string nodeName)
         {
-            var capacity = this.List().FirstOrDefault(x => x.Name() == nodeName)?.Status.Capacity;
+            var capacity = GetNodeCapacity(nodeName);
             var pods     = capacity?.FirstOrDefault(x => x.Key == "pods").Value.CanonicalizeString();
             var current  = GetPodCount(nodeName);
             return $"{current}/{pods}";
         }
+
+        private IDictionary<string, ResourceQuantity> GetNodeCapacity(string nodeName)
+        {
+            var capacity = this.List().FirstOrDefault(x => x.Name() == nodeName)?.Status.Capacity;
+            return capacity;
+        }
+
+        public string GetCpuCapacity(string nodeName)
+        {
+            var capacity = GetNodeCapacity(nodeName);
+            return ResourceCurrentAndCapacity(nodeName,"cpu");
+        }
+        public string GetMemoryCapacity(string nodeName)
+        {
+            var capacity = GetNodeCapacity(nodeName);
+            return ResourceCurrentAndCapacity(nodeName,"memory");
+        }
+
+        private string ResourceCurrentAndCapacity(string nodeName,string type)
+        {
+            var capacity = GetNodeCapacity(nodeName);
+            var all   = capacity?.FirstOrDefault(x => x.Key == type).Value.HumanizeValue();
+
+            var current = MetricsService.NodeMetricsQueue(nodeName).GetList()
+                .FirstOrDefault()?.Usage
+                .FirstOrDefault(x => x.Key == type).Value.HumanizeValue();
+
+            return $"{current}/{all}";
+        }
+
     }
 }
