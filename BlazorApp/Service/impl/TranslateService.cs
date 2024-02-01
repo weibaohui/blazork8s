5769681f1ff8b6e2bae7e278093e324998826c8c
weibaohui <weibaohui@yeah.net>
2024-02-01 08:42:13 +0800

    update

--- BlazorApp/Service/impl/TranslateService.cs
+++ BlazorApp/Service/impl/TranslateService.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using BlazorApp.Service.AI;
@@ -10,13 +11,12 @@ using Microsoft.Extensions.Logging;
 
 namespace BlazorApp.Service.impl;
 
-public class TranslateService(IAiService aiService,ILogger<TranslateService> logger) : ITranslateService
+public class TranslateService(IAiService aiService, ILogger<TranslateService> logger) : ITranslateService
 {
-
     public async Task<string> Translate(string text)
     {
         logger.LogInformation("translate {key}", text);
-        var cnTask = await aiService.AIChat("请完整的将文字翻译为中文，请不要遗漏细节。正文如下："+text);
+        var cnTask = await aiService.AIChat("请完整的将文字翻译为中文，请不要遗漏细节。正文如下：" + text);
         logger.LogInformation("translate {key} over", cnTask);
         return cnTask;
     }
@@ -26,26 +26,34 @@ public class TranslateService(IAiService aiService,ILogger<TranslateService> log
     /// </summary>
     public async Task ProcessAll()
     {
+        // 配置序列化选项
+        var options = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            WriteIndented          = true // 如果需要格式化输出，可以设置为true
+        };
         var entity = SwaggerHelper.Instance.GetAllEntity();
         if (entity.definitions == null) return;
         foreach (var (key, definition) in entity.definitions)
         {
             definition.descriptionCN = RandomNumberGenerator.GetInt32(1, 9999).ToString();
             definition.descriptionCN = await Translate(definition.description);
-            Thread.Sleep(1000*2);
-            await File.WriteAllTextAsync("new.json", JsonSerializer.Serialize(entity));
+            Thread.Sleep(1000 * 2);
+            //及时保存，避免被中断
+            await File.WriteAllTextAsync("new.json", JsonSerializer.Serialize(entity, options));
+
             if (definition.properties == null) continue;
             foreach (var (s, value) in definition.properties)
             {
                 value.descriptionCN = RandomNumberGenerator.GetInt32(10000, 99999).ToString();
                 value.descriptionCN = await Translate(value.description);
-                Thread.Sleep(1000*2);
-                await File.WriteAllTextAsync("new.json", JsonSerializer.Serialize(entity));
+                Thread.Sleep(1000 * 2);
+                //及时保存，避免被中断
+                await File.WriteAllTextAsync("new.json", JsonSerializer.Serialize(entity, options));
             }
         }
 
 
         Console.WriteLine(JsonSerializer.Serialize(entity));
-
     }
 }
