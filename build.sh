71b353cea2f9eeb3365a530e050674dee8fd187a
weibaohui <weibaohui@yeah.net>
2024-02-03 14:32:32 +0800

    0.0.6

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ echo $CR_PAT | docker login ghcr.io -u weibaohui --password-stdin
 
 #docker build -t ghcr.io/weibaohui/blazork8s:latest -f BlazorApp/Dockerfile .
 #docker push ghcr.io/weibaohui/blazork8s:latest
-docker buildx build -t  ghcr.io/weibaohui/blazork8s:0.0.5  -f BlazorApp/Dockerfile   --platform=linux/arm64,linux/amd64 . --push
+docker buildx build -t  ghcr.io/weibaohui/blazork8s:0.0.6  -f BlazorApp/Dockerfile   --platform=linux/arm64,linux/amd64 . --push
