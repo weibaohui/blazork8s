60383f17b53295a620b54e5b7166f48ccfd9ee4f
weibaohui <weibaohui@yeah.net>
2024-02-22 11:33:53 +0800

    Add Cr Instance List

--- BlazorApp/Pages/Crd/CrdDetailView.razor.cs
+++ BlazorApp/Pages/Crd/CrdDetailView.razor.cs
@@ -1,16 +1,67 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using BlazorApp.Pages.Common;
+using BlazorApp.Service.k8s;
+using k8s;
 using k8s.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp.Pages.Crd
 {
-    public partial class CrdDetailView :  DrawerPageBase<V1CustomResourceDefinition>
+    public partial class CrdDetailView : DrawerPageBase<V1CustomResourceDefinition>
     {
+        [Inject]
+        public IKubeService KubeService { get; set; }
+
         private V1CustomResourceDefinition CustomResourceDefinition { get; set; }
+
+        private readonly List<CustomResource> _crInstanceList = new List<CustomResource>();
+
         protected override async Task OnInitializedAsync()
         {
             CustomResourceDefinition = base.Options;
+
+            var group    = CustomResourceDefinition.Spec.Group;
+            var versions = CustomResourceDefinition.Spec.Versions;
+            var plural   = CustomResourceDefinition.Spec.Names.Plural;
+            foreach (var version in versions)
+            {
+                var generic = new GenericClient(KubeService.Client(), group, version.Name, plural);
+                var list    = await generic.ListAsync<CustomResourceList<CustomResource>>();
+                _crInstanceList.AddRange(list.Items);
+            }
+
+            // foreach (var cr in list.Items)
+            // {
+            //     var customObject = await KubeService.Client()
+            //         .GetClusterCustomObjectAsync("kwok.x-k8s.io", "v1alpha1", "stages", cr.Metadata.Name);
+            //  }
+
             await base.OnInitializedAsync();
+        }
+
+
+        public class CustomResource : KubernetesObject, IMetadata<V1ObjectMeta>
+        {
+            [JsonPropertyName("metadata")]
+            public V1ObjectMeta Metadata { get; set; }
+        }
+
+        public abstract class CustomResource<TSpec, TStatus> : CustomResource
+        {
+            [JsonPropertyName("spec")]
+            public TSpec Spec { get; set; }
+
+            [JsonPropertyName("status")]
+            public TStatus Status { get; set; }
+        }
+
+        public class CustomResourceList<T> : KubernetesObject
+            where T : CustomResource
+        {
+            public V1ListMeta Metadata { get; set; }
+            public List<T>    Items    { get; set; }
         }
     }
 }
--- BlazorApp/Pages/Crd/CrdIndex.razor.cs
+++ BlazorApp/Pages/Crd/CrdIndex.razor.cs
@@ -7,26 +7,56 @@ using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp.Pages.Crd;
 
-public partial class CrdIndex: TableBase<V1CustomResourceDefinition>
+public partial class CrdIndex : TableBase<V1CustomResourceDefinition>
 {
-[Inject]
-private ICustomResourceDefinitionService CustomResourceDefinitionService { get; set; }
-private async Task OnResourceChanged(ResourceCache<V1CustomResourceDefinition> data)
-{
-    ItemList = data;
-    TableData.CopyData(ItemList);
-    await InvokeAsync(StateHasChanged);
-}
-protected override async Task OnInitializedAsync()
-{
-    await base.OnInitializedAsync();
-    TableData.CopyData(ItemList);
-    await InvokeAsync(StateHasChanged);
-}
-private async Task OnItemNameClick(V1CustomResourceDefinition item)
-{
-    await PageDrawerHelper<V1CustomResourceDefinition>.Instance
-        .SetDrawerService(PageDrawerService.DrawerService)
-        .ShowDrawerAsync<CrdDetailView, V1CustomResourceDefinition, bool>(item);
-}
+    [Inject]
+    public IKubeService KubeService { get; set; }
+
+    [Inject]
+    private ICustomResourceDefinitionService CustomResourceDefinitionService { get; set; }
+
+    private async Task OnResourceChanged(ResourceCache<V1CustomResourceDefinition> data)
+    {
+        ItemList = data;
+        TableData.CopyData(ItemList);
+        await InvokeAsync(StateHasChanged);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        TableData.CopyData(ItemList);
+        await InvokeAsync(StateHasChanged);
+    }
+    //
+    // private async Task<string> GetCrCount(V1CustomResourceDefinition context)
+    // {
+    //     var ret = string.Empty;
+    //
+    //     // var ret      = new Dictionary<string, int>();
+    //     var group    = context.Spec.Group;
+    //     var versions = context.Spec.Versions;
+    //     var plural   = context.Spec.Names.Plural;
+    //
+    //     foreach (var v in versions)
+    //     {
+    //         var generic = new GenericClient(KubeService.Client(), group, v.Name, plural);
+    //         var list    = await generic.ListAsync<CustomResourceList<CustomResource>>();
+    //         // ret.Add(v.Name, list.Items.Count);
+    //         ret += $"{v.Name}={list.Items.Count}, ";
+    //     }
+    //
+    //     //
+    //     // return ret;
+    //     return ret;
+    // }
+
+    private async Task OnItemNameClick(V1CustomResourceDefinition item)
+    {
+        await PageDrawerHelper<V1CustomResourceDefinition>.Instance
+            .SetDrawerService(PageDrawerService.DrawerService)
+            .ShowDrawerAsync<CrdDetailView, V1CustomResourceDefinition, bool>(item);
+    }
+
+
 }
