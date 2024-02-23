59b7920d0cb58f9c2bd13ea531bc9476cb9f4c41
weibaohui <weibaohui@yeah.net>
2024-02-23 15:01:20 +0800

    ai chat

--- BlazorApp/Pages/CronJob/CronJobIndex.razor.cs
+++ BlazorApp/Pages/CronJob/CronJobIndex.razor.cs
@@ -1,9 +1,11 @@
 using System.Threading.Tasks;
+using BlazorApp.Pages.ai;
 using BlazorApp.Pages.Common;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Pages.CronJob;
 public partial class CronJobIndex : TableBase<V1CronJob>
 {
@@ -26,5 +28,11 @@ public partial class CronJobIndex : TableBase<V1CronJob>
         await PageDrawerHelper<V1CronJob>.Instance
             .SetDrawerService(PageDrawerService.DrawerService)
             .ShowDrawerAsync<CronJobDetailView, V1CronJob, bool>(item);
+    }
+
+    private async Task OnAnyChatClick(string item)
+    {
+        var options = PageDrawerService.DefaultOptions($"AI Chat", width: 1000);
+        await PageDrawerService.ShowDrawerAsync<AnyChat, string, bool>(options, item);
     }
 }
