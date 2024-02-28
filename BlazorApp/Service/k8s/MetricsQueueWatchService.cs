28c6fd3b35c790c5d3432ede3011e38accffbe3e
weibaohui <weibaohui@yeah.net>
2024-02-28 09:58:26 +0800

    clean

--- BlazorApp/Service/k8s/MetricsQueueWatchService.cs
+++ BlazorApp/Service/k8s/MetricsQueueWatchService.cs
@@ -9,26 +9,16 @@ using Microsoft.Extensions.Logging;
 
 namespace BlazorApp.Service.k8s;
 
-public class MetricsQueueWatchService : IHostedService, IDisposable
+public class MetricsQueueWatchService(
+    IKubeService                      kubeService,
+    ILogger<MetricsQueueWatchService> logger,
+    IMetricsService                   metricsService)
+    : IHostedService, IDisposable
 {
-    private readonly ILogger<MetricsQueueWatchService> _logger;
-
-    private readonly IKubeService    _kubeService;
-    private readonly IMetricsService _metricsService;
-
-
-    public MetricsQueueWatchService(IKubeService kubeService, ILogger<MetricsQueueWatchService> logger,
-        IMetricsService                          metricsService)
-    {
-        _kubeService    = kubeService;
-        _logger         = logger;
-        _metricsService = metricsService;
-    }
-
 
     private async void Watch()
     {
-        var ready = await _metricsService.MetricsServerReady();
+        var ready = await metricsService.MetricsServerReady();
         if (!ready)
         {
             // _logger.LogInformation("Metrics not ready");
@@ -36,34 +26,33 @@ public class MetricsQueueWatchService : IHostedService, IDisposable
         }
 
 
-        foreach (var metrics in await _metricsService.PodMetrics())
+        foreach (var metrics in await metricsService.PodMetrics())
         {
             var queue = MetricsQueueHelper<PodMetrics>.Instance.Build(metrics.Name());
             queue.Enqueue(metrics);
         }
 
-        foreach (var metrics in await _metricsService.NodeMetrics())
+        foreach (var metrics in await metricsService.NodeMetrics())
         {
             var queue = MetricsQueueHelper<NodeMetrics>.Instance.Build(metrics.Name());
             queue.Enqueue(metrics);
         }
-}
+    }
 
-public Task StartAsync(CancellationToken cancellationToken)
-{
-    //每1秒执行一次指标更新
-    JobManager.Initialize();
-    JobManager.AddJob(Watch, (s) => s.ToRunEvery(1).Seconds());
-    return Task.CompletedTask;
-}
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        //每1秒执行一次指标更新
+        JobManager.Initialize();
+        JobManager.AddJob(Watch, (s) => s.ToRunEvery(1).Seconds());
+        return Task.CompletedTask;
+    }
 
-public Task StopAsync(CancellationToken cancellationToken)
-{
-    return Task.CompletedTask;
-}
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
 
-public void Dispose()
-{
-}
-
+    public void Dispose()
+    {
+    }
 }
