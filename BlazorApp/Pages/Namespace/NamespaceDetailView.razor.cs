21fc48cd9e809a70e50f490324c265b3b5fffdca
weibaohui <weibaohui@yeah.net>
2024-03-04 15:43:12 +0800

    Namespace Detail Page Add Pod List

--- BlazorApp/Pages/Namespace/NamespaceDetailView.razor.cs
+++ BlazorApp/Pages/Namespace/NamespaceDetailView.razor.cs
@@ -1,15 +1,23 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using BlazorApp.Pages.Common;
+using BlazorApp.Service.k8s;
 using k8s.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp.Pages.Namespace
 {
     public partial class NamespaceDetailView :  DrawerPageBase<V1Namespace>
     {
-        private V1Namespace Namespace { get; set; }
+        [Inject]
+        private IPodService PodService { get; set; }
+
+        private IList<V1Pod> PodList   { get; set; } = new List<V1Pod>();
+        private V1Namespace  Namespace { get; set; }
         protected override async Task OnInitializedAsync()
         {
             Namespace = base.Options;
+            PodList=  PodService.ListByNamespace(Namespace.Name());
             await base.OnInitializedAsync();
         }
     }
