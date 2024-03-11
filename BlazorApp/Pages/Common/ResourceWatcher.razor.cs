3998560d7e6e40c3ba0b6fbdbb44350ac6c3f25e
weibaohui <weibaohui@yeah.net>
2024-03-11 17:34:51 +0800

    add todo

--- BlazorApp/Pages/Common/ResourceWatcher.razor.cs
+++ BlazorApp/Pages/Common/ResourceWatcher.razor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using BlazorApp.Utils;
 using Entity;
@@ -10,21 +11,23 @@ namespace BlazorApp.Pages.Common;
 
 public partial class ResourceWatcher<T> : ComponentBase where T : IKubernetesObject<V1ObjectMeta>
 {
+    private ResourceCache<T> _cache = ResourceCacheHelper<T>.Instance.Build();
+
     [Inject]
     protected NavigationManager MyUriHelper { get; set; }
 
     [Parameter]
     public EventCallback<ResourceCache<T>> OnResourceChanged { get; set; }
+
     [Parameter]
     public EventCallback<PodLogEntity> OnPodLogChanged { get; set; }
 
-    private ResourceCache<T> _cache = ResourceCacheHelper<T>.Instance.Build();
-
     private async Task UpdateMessage(ResourceWatchEntity<T> data)
     {
-        // Console.WriteLine("page收到消息" + data.Message);
+        Console.WriteLine("page收到消息" + data.Message);
         await OnResourceChanged.InvokeAsync(_cache);
     }
+
     private async Task UpdatePodLog(PodLogEntity data)
     {
         // Console.WriteLine("page收到日志" + data.LogLineContent);
@@ -34,6 +37,7 @@ public partial class ResourceWatcher<T> : ComponentBase where T : IKubernetesObj
     protected override async Task OnInitializedAsync()
     {
         var conn = await UIEventHub.Instance().Build(MyUriHelper.ToAbsoluteUri("/chathub"));
+        //todo 一种类型维持一个链接，否则只有一个页面能进行页面响应，其他的不响应。bug
         conn.On<ResourceWatchEntity<T>>("ReceiveMessage", UpdateMessage);
         conn.On<PodLogEntity>("PodLog", UpdatePodLog);
     }
