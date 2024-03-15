a9348d4801f92c5f6b582afc60ad917e2bedb852
weibaohui <weibaohui@yeah.net>
2024-03-15 22:53:39 +0800

    Qwen AI add model config

--- BlazorApp/Service/AI/impl/QwenAIService.cs
+++ BlazorApp/Service/AI/impl/QwenAIService.cs
@@ -1,25 +1,22 @@
 using System;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using BlazorApp.Service.AI.ResponseModels.Qwen;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace BlazorApp.Service.AI.impl;
 
 public class QwenAiService(IConfigService configService, ILogger<QwenAiService> logger) : IQwenAiService
 {
-    private string _incrementResp = "";
-    private string _lastResp      = "";
-
-    string sseEndpoint =
-        "https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation";
+    private const string SseEndpoint = "https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation";
+    private       string _incrementResp = "";
+    private       string _lastResp = "";
 
     public async Task<string> AIChat(string prompt)
     {
-        prompt = JsonConvert.SerializeObject(prompt).TrimStart('"').TrimEnd('"');
         return await Query(prompt);
     }
 
@@ -30,23 +27,26 @@ public class QwenAiService(IConfigService configService, ILogger<QwenAiService> 
 
     public async Task<string> ExplainError(string text)
     {
-        var prompt = configService.GetSection("QwenAI")!.GetSection("Prompt").GetValue<string>("error");
-        text = JsonConvert.SerializeObject(text).TrimStart('"').TrimEnd('"');
-        var content = $"{prompt}:{text}";
+        var prompt  = configService.GetSection("QwenAI")!.GetSection("Prompt").GetValue<string>("error");
+        var content = $"{prompt} \n {text}";
         return await Query(content);
     }
 
     public async Task<string> ExplainSecurity(string text)
     {
-        var prompt = configService.GetSection("QwenAI")!.GetSection("Prompt").GetValue<string>("security");
-        text = JsonConvert.SerializeObject(text).TrimStart('"').TrimEnd('"');
-        var content = $"{prompt}:{text}";
+        var prompt  = configService.GetSection("QwenAI")!.GetSection("Prompt").GetValue<string>("security");
+        var content = $"{prompt} \n {text}";
         return await Query(content);
     }
 
     public string Name()
     {
         return "阿里通义千问大模型";
+    }
+
+    private string GetModel()
+    {
+        return configService.GetString("QwenAI", "Model") ?? string.Empty;
     }
 
     private event EventHandler<string> ChatEventHandler;
@@ -59,23 +59,26 @@ public class QwenAiService(IConfigService configService, ILogger<QwenAiService> 
 
     private async Task<string> Query(string promptAndText)
     {
+        promptAndText = JsonSerializer.Serialize(promptAndText).TrimStart('"').TrimEnd('"');
+
         var       resp       = "";
         using var httpClient = new HttpClient();
-        var       request    = new HttpRequestMessage(HttpMethod.Post, sseEndpoint);
+        var       request    = new HttpRequestMessage(HttpMethod.Post, SseEndpoint);
         request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/event-stream"));
         request.Headers.Add("Authorization", "Bearer " + GetApiKey());
-        var json = """
-                   {
-                       "model": "qwen-1.8b-chat",
-                       "input": {
-                           "prompt": "${promptAndText}"
-                       },
-                       "parameters": {
-                       }
-                   }
-                   """;
-        request.Content = new StringContent(json.Replace("${promptAndText}", promptAndText), Encoding.UTF8,
-            "application/json");
+        const string json = """
+                            {
+                                "model": "${model}",
+                                "input": {
+                                    "prompt": "${promptAndText}"
+                                },
+                                "parameters": {
+                                }
+                            }
+                            """;
+        var jsonStr = json.Replace("${promptAndText}", promptAndText)
+            .Replace("${model}", GetModel());
+        request.Content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
         using var       response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
         await using var body     = await response.Content.ReadAsStreamAsync();
         using var       reader   = new System.IO.StreamReader(body);
@@ -87,12 +90,12 @@ public class QwenAiService(IConfigService configService, ILogger<QwenAiService> 
             }
             else if (line.StartsWith("data:"))
             {
-                var     eventData    = line["data:".Length..].Trim();
-                JObject jsonObj      = JObject.Parse(eventData);
-                var     finishReason = (string)jsonObj["output"]?["finish_reason"];
-                var     text         = (string)jsonObj["output"]?["text"];
-                if (finishReason == "stop")
+                var eventData    = line["data:".Length..].Trim();
+                var chatResponse = JsonSerializer.Deserialize<Response>(eventData);
+                var text         = chatResponse?.Output?.Text;
+                if (chatResponse?.Output?.FinishReason == "stop")
                 {
+                    //阿里通义千问最后一次返回全部
                     resp = text;
                 }
 
@@ -114,17 +117,17 @@ public class QwenAiService(IConfigService configService, ILogger<QwenAiService> 
 
     private void IncrementHandler(string resp)
     {
-        if (!string.IsNullOrWhiteSpace(_lastResp))
-        {
-            _incrementResp = resp.Replace(_lastResp, "");
-        }
-        else
+        if (string.IsNullOrWhiteSpace(_lastResp))
         {
             _incrementResp = resp;
         }
+        else
+        {
+            _incrementResp = resp.Replace(_lastResp, "");
+        }
 
         _lastResp = resp;
-        logger.LogInformation("{Ai} _incrementResp: {IncrementResp}", Name(), _incrementResp);
-        if (ChatEventHandler != null) ChatEventHandler(this, _incrementResp);
+        // logger.LogInformation("{Ai} _incrementResp: {IncrementResp}", Name(), _incrementResp);
+        ChatEventHandler?.Invoke(this, _incrementResp);
     }
 }
