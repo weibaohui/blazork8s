fd8df9be628db4f72edabac6c6bf6d629f26c40d
weibaohui <weibaohui@yeah.net>
2024-03-18 19:51:16 +0800

    update

--- BlazorApp/Pages/Cluster/Metrics.razor.cs
+++ BlazorApp/Pages/Cluster/Metrics.razor.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Timers;
+using BlazorApp.Service;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils.Prometheus.Models.Interfaces;
 using Microsoft.AspNetCore.Components;
@@ -9,17 +10,21 @@ namespace BlazorApp.Pages.Cluster;
 
 public partial class Metrics : ComponentBase
 {
-    [Inject]
-    public IKubeService KubeService { get; set; }
+    private bool  _apiServerRequestDetailVisible        = false;
+    private bool  _apiServerRequestLatencyDetailVisible = false;
+    private bool  _apiServerRequestSliDetailVisible     = false;
+    private bool  _apiServerResponseSizeDetailVisible   = false;
+    private bool  _etcdRequestLatencyDetailVisible      = false;
+    private bool  _etcdStorageDetailVisible             = false;
     private Timer _timer;
 
+    [Inject]
+    private ITranslateService TranslateService { get; set; }
+
+    [Inject]
+    public IKubeService KubeService { get; set; }
+
     private IList<IMetric> AllMetrics { get; set; }
-    private bool _apiServerRequestDetailVisible        = false;
-    private bool _apiServerRequestLatencyDetailVisible = false;
-    private bool _apiServerResponseSizeDetailVisible   = false;
-    private bool _apiServerRequestSliDetailVisible     = false;
-    private bool _etcdStorageDetailVisible             = false;
-    private bool _etcdRequestLatencyDetailVisible      = false;
 
 
     protected override async Task OnInitializedAsync()
@@ -29,6 +34,7 @@ public partial class Metrics : ComponentBase
         _timer.Start();
         await OnTimerCallback(); //先执行一次
         await base.OnInitializedAsync();
+        await TranslateService.ProcessKubeExplains();
     }
 
     private async Task OnTimerCallback()
