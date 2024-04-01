1e4824ed821df55cc524e21388f093db1a6a00d9
weibaohui <weibaohui@yeah.net>
2024-04-01 12:06:53 +0800

    update pod log locig

--- BlazorApp/Pages/Pod/PodLogsView.razor.cs
+++ BlazorApp/Pages/Pod/PodLogsView.razor.cs
@@ -1,19 +1,19 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AntDesign;
-using BlazorApp.Chat;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils;
-using Entity;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.SignalR;
 using XtermBlazor;
 
 namespace BlazorApp.Pages.Pod;
 
-public partial class PodLogsView : FeedbackComponent<V1Pod, bool>
+public partial class PodLogsView : FeedbackComponent<V1Pod, bool>, IDisposable
 {
+    private readonly CancellationTokenSource _gracefulCts = new();
+
     private readonly TerminalOptions _options = new()
     {
         CursorBlink = true,
@@ -22,17 +22,15 @@ public partial class PodLogsView : FeedbackComponent<V1Pod, bool>
         Rows        = 50,
         Theme =
         {
-            Background = "#17615e",
-        },
+            Background = "#17615e"
+        }
     };
 
+    private string _command = "";
 
-    private int            _columns, _rows;
-    private string         _containerName;
-    private bool           _follow;
-    private bool           _ignoreErrors;
-    private PodLogExecutor _logHelper;
-
+    private string    _containerName;
+    private bool      _follow;
+    private bool      _ignoreErrors;
     private V1Pod     _podItem;
     private string    _podRunningTimeout = "";
     private bool      _prefix;
@@ -42,30 +40,50 @@ public partial class PodLogsView : FeedbackComponent<V1Pod, bool>
     private DateTime? _sinceTimestamp;
     private string    _tail = "";
 
+
     private Xterm _terminal;
+
+    private PodLogCommandBuilder commander;
+
+    [Inject]
+    private IKubectlService Kubectl { get; set; }
 
     [Inject]
     private IPodService PodService { get; set; }
 
-    [Inject]
-    private IHubContext<ChatHub> Ctx { get; set; }
+    public new void Dispose()
+    {
+        _gracefulCts.CancelAsync();
+    }
+
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
         _podItem       = base.Options;
         _containerName = _podItem.Spec.Containers[0].Name;
-        _logHelper = new PodLogExecutorHelper().GetOrCreate(_podItem.Namespace(), _podItem.Name(), _containerName)
-            .SetHubContext(Ctx);
+        Kubectl.SetOutputEventHandler(EventHandler);
+        Kubectl.SetCancellationToken(_gracefulCts.Token);
+        commander = new PodLogCommandBuilder();
     }
+
+    private async void EventHandler(object sender, string resp)
+    {
+        await _terminal.WriteLine(resp);
+        await InvokeAsync(StateHasChanged);
+    }
+
 
     private async Task OnOkBtnClicked()
     {
         await _terminal.Clear();
-        var _ = _containerName == "all-containers"
-            ? _logHelper.SetAllContainers(true)
-            : _logHelper.SetAllContainers(false);
-        _logHelper.SetShowTimestamp(_showTimestamp)
+
+        commander
+            .SetNamespace(_podItem.Namespace())
+            .SetPodName(_podItem.Name())
+            .SetContainerName(_containerName)
+            .SetAllContainers(_containerName == "all-containers")
+            .SetShowTimestamp(_showTimestamp)
             .SetSinceTimestamp(_sinceTimestamp)
             .SetFollow(_follow)
             .SetPrefix(_prefix)
@@ -74,8 +92,9 @@ public partial class PodLogsView : FeedbackComponent<V1Pod, bool>
             .SetTail(_tail)
             .SetPodRunningTimeout(_podRunningTimeout)
             .SetIgnoreErrors(_ignoreErrors);
-        _logHelper.BuildLogCommand();
-        await _logHelper.StartLog();
+        commander.Build();
+        _command = commander.GetCommand();
+        if (!string.IsNullOrWhiteSpace(_command)) await Kubectl.Command(_command);
     }
 
     private void OnContainerSelectChanged(string name)
@@ -83,29 +102,6 @@ public partial class PodLogsView : FeedbackComponent<V1Pod, bool>
         _containerName = name;
     }
 
-
-    private async Task OnFirstRender()
-    {
-        // Blazor Server
-        // await _terminal.InvokeAddonFunctionVoidAsync("xterm-addon-attach", "new AttachAddon()");
-        // await _terminal.InvokeAddonFunctionVoidAsync("xterm-addon-fit", "fit");
-
-        _columns = await _terminal.GetColumns();
-        _rows    = await _terminal.GetRows();
-    }
-
-
-    /// <summary>
-    /// 接收到SignalR后端发送过来的日志，回显到Xterm
-    /// </summary>
-    /// <param name="obj"></param>
-    private async Task OnPodLogChanged(PodLogEntity obj)
-    {
-        if (obj.Namespace == _podItem.Namespace() && obj.Name == _podItem.Name())
-        {
-            await _terminal.WriteLine(obj.LogLineContent);
-        }
-    }
 
     #region Getter Setter
 
---
+++ BlazorApp/Utils/PodLogCommandBuilder.cs
@@ -0,0 +1,152 @@
+#nullable enable
+using System;
+using Extension;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorApp.Utils;
+
+public class PodLogCommandBuilder
+{
+    private static readonly ILogger<PodLogCommandBuilder> Logger         = LoggingHelper<PodLogCommandBuilder>.Logger();
+    private                 bool                          _allContainers = false;
+    private                 string                        _command;
+    private                 string                        _containerName;
+    private                 bool                          _follow;
+    private                 bool                          _ignoreErrors;
+    private                 string                        _namespace;
+
+    private string    _podName;
+    private string    _podRunningTimeout;
+    private bool      _prefix;
+    private bool      _previous;
+    private bool      _showTimestamp;
+    private string    _since;
+    private DateTime? _sinceTimestamp;
+    private string    _tail;
+
+
+    public PodLogCommandBuilder Build()
+    {
+        _command = "";
+        var extCmd = "";
+        if (_follow)
+            extCmd += " --follow=true ";
+        if (_prefix)
+            extCmd += " --prefix=true ";
+        if (_previous)
+            extCmd += " --previous=true ";
+        if (_ignoreErrors)
+            extCmd += " --ignore-errors=true ";
+        if (_showTimestamp)
+            extCmd += " --timestamps=true ";
+        if (!_podRunningTimeout.IsNullOrEmpty())
+            extCmd += $" --pod-running-timeout={_podRunningTimeout} ";
+        if (!_tail.IsNullOrEmpty())
+            extCmd += $" --tail={_tail} ";
+
+        //设置了since并且不是指定
+        if (!_since.IsNullOrEmpty() && _since != "specified") extCmd += $" --since={_since} ";
+
+        //只设置了sinceTimestamp的情况
+        if (_since.IsNullOrEmpty() && _sinceTimestamp != null)
+            extCmd += $" --since-time='{new DateTimeOffset(_sinceTimestamp.Value):yyyy-MM-dd'T'HH:mm:ss.fffK}' ";
+
+        //since设置为指定，同时也设置了sinceTimestamp的情况
+        if (!_since.IsNullOrEmpty() && _since == "specified" && _sinceTimestamp != null)
+            extCmd += $" --since-time='{new DateTimeOffset(_sinceTimestamp.Value):yyyy-MM-dd'T'HH:mm:ss.fffK}' ";
+
+        //是否显示所有容器
+        extCmd += _allContainers ? " --all-containers=true " : $" -c {_containerName}  ";
+
+        _command = $"logs -n {_namespace} {_podName} {extCmd}";
+        return this;
+    }
+
+
+    #region Getter Setter
+
+    public string? GetCommand()
+    {
+        return _command;
+    }
+
+    public PodLogCommandBuilder SetNamespace(string ns)
+    {
+        _namespace = ns;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetPodName(string podName)
+    {
+        _podName = podName;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetContainerName(string containerName)
+    {
+        _containerName = containerName;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetAllContainers(bool flag)
+    {
+        _allContainers = flag;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetShowTimestamp(bool showTimestamp)
+    {
+        _showTimestamp = showTimestamp;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetFollow(bool follow)
+    {
+        _follow = follow;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetPrefix(bool prefix)
+    {
+        _prefix = prefix;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetPrevious(bool previous)
+    {
+        _previous = previous;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetIgnoreErrors(bool ignoreErrors)
+    {
+        _ignoreErrors = ignoreErrors;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetSinceTimestamp(DateTime? sinceTimestamp)
+    {
+        _sinceTimestamp = sinceTimestamp;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetSince(string since)
+    {
+        _since = since;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetPodRunningTimeout(string timeout)
+    {
+        _podRunningTimeout = timeout;
+        return this;
+    }
+
+    public PodLogCommandBuilder SetTail(string tail)
+    {
+        _tail = tail;
+        return this;
+    }
+
+    #endregion
+}
