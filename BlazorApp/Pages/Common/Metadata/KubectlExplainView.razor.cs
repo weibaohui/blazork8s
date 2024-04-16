539eb22d22240ab92b7f3342cb5032a0f45b6409
weibaohui <weibaohui@yeah.net>
2024-04-16 15:16:20 +0800

    文档翻译目前为中文，暂时屏蔽其他语言

--- BlazorApp/Pages/Common/Metadata/KubectlExplainView.razor.cs
+++ BlazorApp/Pages/Common/Metadata/KubectlExplainView.razor.cs
@@ -2,7 +2,6 @@ using System.Threading.Tasks;
 using BlazorApp.Service.AI;
 using BlazorApp.Service.k8s;
 using Microsoft.AspNetCore.Components;
-using BlazorApp.Pages.Common;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 
@@ -11,23 +10,18 @@ namespace BlazorApp.Pages.Common.Metadata;
 public partial class KubectlExplainView : DrawerPageBase<string>
 
 {
-    [Parameter]
-    public string Field { get; set; }
-
-    [Inject]
-    IKubectlService Kubectl { get; set; }
-
-    [Inject]
-    IDocService DocService { get; set; }
-
-    [Inject]
-    IAiService AiService { get; set; }
-
-    [Inject]
-    ILogger<KubectlExplainView> Logger { get; set; }
-
     private string _result;
     private string _resultCn;
+
+    [Parameter] public string Field { get; set; }
+
+    [Inject] private IKubectlService Kubectl { get; set; }
+
+    [Inject] private IDocService DocService { get; set; }
+
+    [Inject] private IAiService AiService { get; set; }
+
+    [Inject] private ILogger<KubectlExplainView> Logger { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -38,32 +32,31 @@ public partial class KubectlExplainView : DrawerPageBase<string>
         //缓存中没有中文，执行AI翻译
         if (!Field.IsNullOrEmpty())
         {
-            var explainEntity =await DocService.GetExplainByField(Field);
-            _result   = explainEntity?.Explain;
+            var explainEntity = await DocService.GetExplainByField(Field);
+            _result = explainEntity?.Explain;
             _resultCn = explainEntity?.ExplainCN;
         }
 
         if (string.IsNullOrWhiteSpace(_result))
         {
-
             Logger.LogWarning("no explain for {Field}", Field);
             //没有解释，执行kubectl explain
             _result = await Kubectl.Explain(Field);
 
             if (string.IsNullOrWhiteSpace(_resultCn))
             {
-                Logger.LogWarning("translate {Field},AI enable:{Enabled}", Field,AiService.Enabled());
+                Logger.LogWarning("translate {Field},AI enable:{Enabled}", Field, AiService.Enabled());
                 //没有中文解释，查询翻译
                 if (AiService.Enabled())
                 {
                     AiService.SetChatEventHandler(EventHandler);
+                    //todo 修改为可翻译当前选择的语言
                     _resultCn = await AiService.AIChat("请逐字翻译以下内容，注意请不要遗漏细节并保持原来的格式：" + _result);
                 }
             }
         }
 
         await base.OnInitializedAsync();
-
     }
 
 
