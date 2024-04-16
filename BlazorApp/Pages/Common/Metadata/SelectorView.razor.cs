0412abfe5315e0397ab48cab5bf50f91e3e76406
weibaohui <weibaohui@yeah.net>
2024-04-16 14:55:09 +0800

    update doc

--- BlazorApp/Pages/Common/Metadata/SelectorView.razor.cs
+++ BlazorApp/Pages/Common/Metadata/SelectorView.razor.cs
@@ -1,14 +1,13 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
-using BlazorApp.Pages.Common;
 
 namespace BlazorApp.Pages.Common.Metadata;
 
 public partial class SelectorView : PageBase
 {
-    [Parameter]
-    public IDictionary<string, string> Selector { get; set; }
+    [Parameter] public IDictionary<string, string> Selector { get; set; }
 
-    [Parameter]
-    public string Title { get; set; }
+    [Parameter] public string Title { get; set; }
+
+    [Parameter] public string ExplainField { get; set; } = "";
 }
