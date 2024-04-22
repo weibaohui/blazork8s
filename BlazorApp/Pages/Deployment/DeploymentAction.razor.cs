3f52777d0bace80d57aa1dc5647726f33aff5594
weibaohui <weibaohui@yeah.net>
2024-04-22 18:11:07 +0800

    update scale page

--- BlazorApp/Pages/Deployment/DeploymentAction.razor.cs
+++ BlazorApp/Pages/Deployment/DeploymentAction.razor.cs
@@ -7,41 +7,33 @@ using BlazorApp.Service.AI;
 using BlazorApp.Service.k8s;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
-using BlazorApp.Pages.Common;
 
 namespace BlazorApp.Pages.Deployment;
 
 public partial class DeploymentAction : PageBase
 {
-    [Inject]
-    public IDeploymentService DeploymentService { get; set; }
-
-    [Inject]
-    ModalService ModalService { get; set; }
-    [Inject]
-    IMessageService MessageService { get; set; }
-
-    [Parameter]
-    public MenuMode MenuMode { get; set; } = MenuMode.Vertical;
-
-    [Parameter]
-    public V1Deployment Item { get; set; }
-
-
-    [Inject]
-    private IAiService Ai { get; set; }
-
-    [Parameter]
-    public EventCallback<V1Deployment> OnDeploymentDelete { get; set; }
-
     public bool Enable;
+
+    [Inject] public IDeploymentService DeploymentService { get; set; }
+
+    [Inject] private ModalService ModalService { get; set; }
+
+    [Inject] private IMessageService MessageService { get; set; }
+
+    [Parameter] public MenuMode MenuMode { get; set; } = MenuMode.Vertical;
+
+    [Parameter] public V1Deployment Item { get; set; }
+
+
+    [Inject] private IAiService Ai { get; set; }
+
+    [Parameter] public EventCallback<V1Deployment> OnDeploymentDelete { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         Enable = Ai.Enabled();
         await base.OnInitializedAsync();
     }
-
 
 
     private async Task OnRestartClick(V1Deployment item)
@@ -54,9 +46,10 @@ public partial class DeploymentAction : PageBase
     {
         var options = new ConfirmOptions()
         {
-            Title        = "Scale Deployment : " + item.Name(),
+            Title = "Scale Deployment : " + item.Name(),
             MaskClosable = true,
-            Mask         = true
+            Mask = true,
+            Width = "600px"
         };
 
         var confirmRef =
