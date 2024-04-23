d50355a6589261289dbbb87bee2ebe51895b3edd
weibaohui <weibaohui@yeah.net>
2024-04-23 14:38:24 +0800

    update ingress list ui display

--- BlazorApp/Pages/Ingress/IngressIndex.razor.cs
+++ BlazorApp/Pages/Ingress/IngressIndex.razor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using BlazorApp.Pages.Common;
 using BlazorApp.Service.k8s;
@@ -9,8 +10,7 @@ namespace BlazorApp.Pages.Ingress;
 
 public partial class IngressIndex : TableBase<V1Ingress>
 {
-    [Inject]
-    private IIngressService IngressService { get; set; }
+    [Inject] private IIngressService IngressService { get; set; }
 
     private async Task OnResourceChanged(ResourceCache<V1Ingress> data)
     {
@@ -31,5 +31,48 @@ public partial class IngressIndex : TableBase<V1Ingress>
         await PageDrawerHelper<V1Ingress>.Instance
             .SetDrawerService(PageDrawerService.DrawerService)
             .ShowDrawerAsync<IngressDetailView, V1Ingress, bool>(item);
+    }
+
+    private static string GetRulePathDisplay(V1IngressRule rule, V1HTTPIngressPath path, IList<V1IngressTLS> specTls)
+    {
+        var pathDisplay = "";
+        if (specTls is { Count: > 0 })
+            pathDisplay += "https://";
+        else
+            pathDisplay += "http://";
+
+        if (rule?.Host != null && !string.IsNullOrWhiteSpace(rule?.Host))
+            pathDisplay += $"{rule.Host}";
+        else
+            pathDisplay += "*";
+
+        if (path?.Path != null) pathDisplay += $"{path.Path}";
+
+
+        return pathDisplay;
+    }
+
+    private static string GetRulePathBackend(V1HTTPIngressPath path)
+    {
+        var pathDisplay = "";
+
+        if (path?.Backend?.Service?.Name != null && !string.IsNullOrWhiteSpace(path.Backend?.Service?.Name))
+            pathDisplay += $"{path.Backend?.Service?.Name}";
+
+        if (path?.Backend?.Service?.Port?.Number != null) pathDisplay += $":{path.Backend?.Service?.Port?.Number}";
+
+        return pathDisplay;
+    }
+
+    private static V1ObjectReference GetRulePathBackend(V1HTTPIngressPath path, string ns)
+    {
+        var reference = new V1ObjectReference
+        {
+            ApiVersion = "v1",
+            Kind = "Service",
+            Name = path?.Backend?.Service?.Name,
+            NamespaceProperty = ns
+        };
+        return reference;
     }
 }
