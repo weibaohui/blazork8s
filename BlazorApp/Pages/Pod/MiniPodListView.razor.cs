ea71e70210dd06282aec8cc8ad95c337752938dc
weibaohui <weibaohui@yeah.net>
2024-04-24 09:46:26 +0800

    clean

--- BlazorApp/Pages/Pod/MiniPodListView.razor.cs
+++ BlazorApp/Pages/Pod/MiniPodListView.razor.cs
@@ -1,44 +1,35 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using AntDesign;
 using BlazorApp.Pages.Common;
 using BlazorApp.Service.k8s;
 using BlazorApp.Utils;
 using k8s.Models;
 using Microsoft.AspNetCore.Components;
 
-namespace BlazorApp.Pages.Pod
+namespace BlazorApp.Pages.Pod;
+
+public partial class MiniPodListView : PageBase
 {
-    public partial class MiniPodListView : PageBase
+    [Inject] private IPodService PodService { get; set; }
+
+    [Parameter] public IList<V1Pod> Pods { get; set; } = new List<V1Pod>();
+
+
+    [Parameter] public string ControllerByUid { get; set; }
+
+
+    protected override async Task OnInitializedAsync()
     {
-        [Inject] private IPodService PodService { get; set; }
+        if (!string.IsNullOrEmpty(ControllerByUid)) Pods = PodService.ListByOwnerUid(ControllerByUid);
 
-        [Inject] private DrawerService DrawerService { get; set; }
-
-        [Parameter] public IList<V1Pod> Pods { get; set; } = new List<V1Pod>();
-
-
-        [Parameter] public string ControllerByUid { get; set; }
-
-        [Parameter] public bool HideAction { get; set; }
+        await base.OnInitializedAsync();
+    }
 
 
-        protected override async Task OnInitializedAsync()
-        {
-            if (!string.IsNullOrEmpty(ControllerByUid))
-            {
-                Pods = PodService.ListByOwnerUid(ControllerByUid);
-            }
-
-            await base.OnInitializedAsync();
-        }
-
-
-        async Task OnPodNameClick(V1Pod pod)
-        {
-            await PageDrawerHelper<V1Pod>.Instance
-                .SetDrawerService(DrawerService)
-                .ShowDrawerAsync<PodDetailView, V1Pod, bool>(pod);
-        }
+    private async Task OnPodNameClick(V1Pod pod)
+    {
+        await PageDrawerHelper<V1Pod>.Instance
+            .SetDrawerService(PageDrawerService.DrawerService)
+            .ShowDrawerAsync<PodDetailView, V1Pod, bool>(pod);
     }
 }
