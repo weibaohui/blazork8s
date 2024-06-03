5f5cb378b5390da884c819b933cba9a58494daf4
weibaohui <weibaohui@yeah.net>
2024-06-03 09:12:47 +0800

    增加Python代码提取

--- BlazorApp/GptWorkflow/WorkflowConst.cs
+++ BlazorApp/GptWorkflow/WorkflowConst.cs
@@ -13,6 +13,7 @@ public class WorkflowConst
     public const string BranchRunEnd = "BranchRunEnd";
     public static string RegexPatternShell = @"```shell(?:[\s\S]*?)```";
     public static string RegexPatternYaml = @"```yaml(?:[\s\S]*?)```";
+    public static string RegexPatternPython = @"```python(?:[\s\S]*?)```";
     public static string RegexPatternKubectl = @"kubectl\s+\w+\s+.*?(?=\r?\n)";
     public static string DecidePASS = "PASS";
     public static string DecideNonePASS = "NonePASS";
